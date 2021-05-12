b8e885a2a8d508d88f2125bb0882f1b5479cf08a
SamarashEugene <samarasha95@gmail.com>
2021-05-12 22:31:34 +0300

    project is ready

--- js/class.js
+++ js/class.js
@@ -1,6 +1,9 @@
 "use strict"
 
 let modalRecall = document.getElementById('modal-recall')
+let hideFormButt = document.getElementById('hideForm')
+
+hideFormButt.addEventListener('click', hideForm)
 
 function showForm() {
     modalRecall.style.display = 'block'
